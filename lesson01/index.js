e11017936f9a1486193dbfa3d8edafd735d1755a
zhangzhengyang <1074385735@qq.com>
2022-05-09 19:16:14 +0800

    first commit webpack

---
+++ lesson01/index.js
@@ -0,0 +1,8 @@
+// ES Moudule 模块引入方式  浏览器不识别 import
+import Header from './header.js';
+import Sidebar from './sidebar.js';
+import Content from './content.js';
+
+new Header();
+new Sidebar();
+new Content();
