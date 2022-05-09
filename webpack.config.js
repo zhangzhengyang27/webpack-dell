3d77ab8463cdcb0a8e6240f28a779221604d3e60
zhangzhengyang <1074385735@qq.com>
2022-05-09 22:55:35 +0800

    配置 url-loader 打包图片

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,17 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.jpg$/,
+            test: /\.(jpg|png|gif)$/,
             use: {
-                loader: 'file-loader'
+                // url-loader 可以替代 file-loader的所有行为
+                loader: 'url-loader',
+                options: {
+                    // name 使用之前的名字  ext 使用之前的后缀名
+                    name: '[name]_[hash].[ext]',
+                    outputPath: 'images/',
+                    // 限制大小 大于的生成图片，否则生成base64的图片
+                    limit: 10240
+                }
             }
         }]
     },
